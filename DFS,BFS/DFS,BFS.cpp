db46dadfbe99bdafc2a2d273d469ce3954c88f58
Serendipity-Song <thdalwh3867@gmail.com>
2021-09-17 20:43:47 +0900

    초기 커밋.

---
+++ DFS,BFS/DFS,BFS.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+#include <queue>
+#include<algorithm>
+
+
+#define x first
+#define y second
+
+using namespace std;
+
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,1,0,-1 };
+int n, m;
+int board[501][501];
+int vis[501][501];
+queue<pair<int, int> > Q;
+
+int main()
+{
+
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; j++) {
+		for (int i = 0; j < m; j++) {
+			cin >> mp[i][j];
+		}
+	}
+	queue < pair<int, int> >Q;
+	int cnt = 0; //그림의 개수
+	int mx = 0; //그림의 넓이
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			if (vis[i][j] \\ mp[i][j] == 0) continue;
+
+		}
+	}
+	
+}
